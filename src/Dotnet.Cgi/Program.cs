fbaf4840a70f8097321d9808f79e86e5e297c29d
Brandon Poe <bpoe@microsoft.com>
2023-05-12 18:33:46 -0700

    Moving code to write response to own class

--- src/Dotnet.Cgi/Program.cs
+++ src/Dotnet.Cgi/Program.cs
@@ -16,17 +16,4 @@ context.Response.StatusCode = HttpStatusCode.BadRequest;
 context.Response.Content = new StringContent(responseContent.ToString(), new MediaTypeHeaderValue("application/json"));
 context.Response.Headers.Add("foo", "bar");
 
-Console.WriteLine("Status: {0} {1}", (int)context.Response.StatusCode, ReasonPhrases.GetReasonPhrase((int)context.Response.StatusCode));
-
-foreach (var h in context.Response.Headers)
-{
-    Console.WriteLine("{0}: {1}", h.Key, string.Concat(h.Value));
-}
-
-foreach (var h in context.Response.Content.Headers)
-{
-    Console.WriteLine("{0}: {1}", h.Key, string.Concat(h.Value));
-}
-
-Console.WriteLine();
-Console.WriteLine(context.Response.Content.ReadAsStringAsync().Result);
+Result.WriteResponse(context.Response);
---
+++ src/Dotnet.Cgi/Result.cs
@@ -0,0 +1,28 @@
+﻿namespace Dotnet.Cgi
+{
+    using System;
+
+    public static class Result
+    {
+        public static void WriteResponse(HttpResponseMessage response)
+        {
+            Console.WriteLine("Status: {0} {1}", (int)response.StatusCode, ReasonPhrases.GetReasonPhrase((int)response.StatusCode));
+
+            foreach (var h in response.Headers)
+            {
+                Console.WriteLine("{0}: {1}", h.Key, string.Concat(h.Value));
+            }
+
+            if (response.Content != null)
+            {
+                foreach (var h in response.Content.Headers)
+                {
+                    Console.WriteLine("{0}: {1}", h.Key, string.Concat(h.Value));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+        }
+    }
+}
